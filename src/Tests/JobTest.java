22e1c35bba002aa644336bc71165599a42929fd6
Zumerah <aymanz2094@gmail.com>
2020-06-14 22:20:44 -0500

    Completed all steps

---
+++ src/Tests/JobTest.java
@@ -0,0 +1,73 @@
+package Tests;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.launchcode.techjobs_oo.*;
+import static org.junit.Assert.*;
+
+public class JobTest {
+    static Job jobOne;
+    static Job jobTwo;
+    static Job jobThree;
+    static Job jobFour;
+    static Job jobFive;
+
+    @BeforeClass
+    public static void createJobObjects() {
+        jobOne = new Job();
+        jobTwo = new Job();
+        jobThree = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        jobFour = new Job("Product tester", new Employer(""), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency(""));
+        jobFive = new Job("Product tester", new Employer(""), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency(""));
+    }
+
+
+    @Test
+    public void testSettingJobId() {
+        assertFalse(jobOne.getId() == jobTwo.getId());
+        assertTrue(jobOne.getId() == jobTwo.getId() -1);
+
+    }
+    @Test
+    public void testJobConstructorSetsAllFields(){
+        Integer I = jobThree.getId();
+        assertTrue(I instanceof Integer);
+        assertTrue(jobThree.getId() == 3);
+        assertTrue(jobThree.getName() instanceof String);
+        assertTrue(jobThree.getName().equals("Product tester"));
+        assertTrue(jobThree.getEmployer() instanceof Employer);
+        assertTrue(jobThree.getEmployer().getValue().equals("ACME"));
+        assertTrue(jobThree.getLocation() instanceof Location);
+        assertTrue(jobThree.getLocation().getValue().equals("Desert"));
+        assertTrue(jobThree.getPositionType() instanceof PositionType);
+        assertTrue(jobThree.getPositionType().getValue().equals("Quality control"));
+        assertTrue(jobThree.getCoreCompetency() instanceof CoreCompetency);
+        assertTrue(jobThree.getCoreCompetency().getValue().equals("Persistence"));
+    }
+
+    @Test
+    public void testToStringStartsWithBlankLine(){
+        assertTrue(jobThree.toString().startsWith("\n"));
+    }
+    @Test
+    public void testToStringEndsWithBlankLine(){
+        assertTrue(jobThree.toString().endsWith("\n"));
+    }
+    @Test
+    public void testToStringFull(){
+        assertEquals("\nID: 3\nName: Product tester\nEmployer: ACME\nLocation: Desert\nPosition Type: Quality control\nCore Competency: Persistence\n",jobThree.toString() );
+
+    }
+    @Test
+    public void testToStringPartialEmpty(){assertEquals("\nID: 4\nName: Product tester\nEmployer: Data not available\nLocation: Desert\nPosition Type: Quality control\nCore Competency: Data not available\n", jobFour.toString());
+
+    }
+    @Test
+    public void testJobsForEquality(){
+        assertFalse(jobFour.equals(jobFive));
+
+    }
+
+}
+
+
--- src/org/launchcode/techjobs_oo/PositionType.java
+++ src/org/launchcode/techjobs_oo/PositionType.java
@@ -1,50 +1,12 @@
 package org.launchcode.techjobs_oo;
 
-public class PositionType {
-    private int id;
-    private static int nextId = 1;
-    private String value;
+import java.util.Objects;
 
+public class PositionType extends JobField{
     public PositionType() {
-        id = nextId;
-        nextId++;
-    }
 
+    }
     public PositionType(String value) {
-        this();
-        this.value = value;
-    }
-
-    // TODO: Add a custom toString() method that returns the data stored in 'value'.
-    @Override
-    public String toString() { return value; }
-    // TODO: Add custom equals and hashCode methods. Consider two PositionType objects "equal" when
-    //  their id fields match.
-
-    public boolean equals(Object object) {
-        if (this == object) return true;
-        if (object == null || getClass() != object.getClass()) return false;
-        if (!super.equals(object)) return false;
-        PositionType that = (PositionType) object;
-        return id == that.id;
-    }
-
-    public int hashCode() {
-        return java.util.Objects.hash(super.hashCode(), id);
-    }
-
-
-    // Getters and Setters:
-
-    public int getId() {
-        return id;
-    }
-
-    public String getValue() {
-        return value;
-    }
-
-    public void setValue(String value) {
-        this.value = value;
+        super(value);
     }
 }
